f00a2e8bdc19bfdfa2f68f324e2c30743799858e
LCH <ckdgus4002@gmail.com>
2023-05-11 21:00:54 +0900

    Commit.

---
+++ Runtime/Scripts/Extensions/Spine/SkeletonGraphicExtension.cs
@@ -0,0 +1,16 @@
+#if Spine
+using Spine.Unity;
+
+namespace LCHFramework.Extensions
+{ 
+    public static class SkeletonGraphicExtension
+    {
+        public static void SetSkinContinuously(this SkeletonGraphic skeletonGraphic, string skinName)
+        {
+            skeletonGraphic.Skeleton.SetSkin(skinName);
+            skeletonGraphic.Skeleton.SetSlotsToSetupPose();
+            skeletonGraphic.initialSkinName = skinName;
+        }
+    }
+}
+#endif
